d37be5fedc2ef121c853782b87376c715e5f6c94
Vedant <vedant28j@gmail.com>
2025-07-10 16:07:24 +0000

    Using Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import NavBar from './component/NavBar'
 import Home from './pages/Home'
 import Product from './pages/Product'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import { Routes, Route } from 'react-router-dom'
 
 
@@ -9,6 +11,12 @@ const App = () => {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/products' element={<Product />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
     </div>
   )
 }
