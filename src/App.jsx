fcaa634ecfa0a49e3747df0beacf149e865f47b1
Vedant <vedant28j@gmail.com>
2025-07-10 16:03:14 +0000

    impoerting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import NavBar from './component/NavBar'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import { Routes, Route } from 'react-router-dom'
+
 
 const App = () => {
   return (
