171cf720c1241e69e76590f9e1ac958ab71fb50b
Vedant <vedant28j@gmail.com>
2025-07-11 01:40:06 +0000

    Changing link to NavLink This adds a special 'active' class to the current link

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import router from '../assets/router.png'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className='navbar'>
         <img src={router} alt="" className='img'/>
         <ul>
-            <Link to='/'><li>Home</li></Link>
-            <Link to='/products'><li>Product</li></Link>
-            <Link to='/about'><li>About</li></Link>
-            <Link to='/contact'><li>Contact</li></Link>
+            <NavLink to='/'><li>Home</li></NavLink>
+            <NavLink to='/products'><li>Product</li></NavLink>
+            <NavLink to='/about'><li>About</li></NavLink>
+            <NavLink to='/contact'><li>Contact</li></NavLink>
         </ul>
         <button>Get Started</button>
     </div>
