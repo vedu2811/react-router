43ce1c2d37abd95d6360583898a6f6fb76b6b5ab
Vedant <vedant28j@gmail.com>
2025-07-11 00:59:35 +0000

    PART - 2: create a const router using createBrowserRouter add all the route to it

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,25 @@ import Home from './pages/Home'
 import Product from './pages/Product'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import { Routes, Route } from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import RootLayout from './layout/RootLayout'
 
 
 const App = () => {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />} >
+        <Route index element={<Home />}/>
+        <Route path='products' element={<Product />}/>
+        <Route path='about' element={<About />}/>
+        <Route path='contact' element={<Contact />}/>
+      </Route>
+    )
+  )
+
   return (
-    <div>
-      <NavBar />
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/products' element={<Product />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/contact' element={<Contact />}/>
-        </Routes>
-      </div>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
