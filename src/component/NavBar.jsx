b30ce2a6a4cb7c7806f20e5cbf290f78616d4ea1
Vedant <vedant28j@gmail.com>
2025-07-11 04:00:07 +0000

    Using useNaviagte Hook

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import router from '../assets/router.png'
 // import { Link } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+
+  const navigate=useNavigate();
+
   return (
     <div className='navbar'>
         <img src={router} alt="" className='img'/>
@@ -13,7 +16,7 @@ const NavBar = () => {
             <NavLink to='/about'><li>About</li></NavLink>
             <NavLink to='/contact'><li>Contact</li></NavLink>
         </ul>
-        <button>Get Started</button>
+        <button onClick={() => navigate('/about', {replace:true})}>Get Started</button>
     </div>
   )
 }
