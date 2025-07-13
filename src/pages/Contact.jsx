04742ea8ec055142773ddb65720af88ea57042ef
Vedant <vedant28j@gmail.com>
2025-07-13 09:53:12 +0000

    Nested Routing - V Adding contact info and form buttons on the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+
+  const navigate=useNavigate();
+
   return (
     <div>
         <h1>Contact Page</h1>
+        <div className='contact-buttons'>
+          <button onClick={() => navigate('info')}>Contact Info</button>
+          <button onClick={() => navigate('form')}>Contact Form</button>
+        </div>
     </div>
   )
 }
