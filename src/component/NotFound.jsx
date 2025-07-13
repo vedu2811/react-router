db4fdcdcc11bc11410e33ec9ca4360a6e94ecb7b
Vedant <vedant28j@gmail.com>
2025-07-13 10:07:12 +0000

    Adding a button to go back to home page from not found page

--- src/component/NotFound.jsx
+++ src/component/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+
+    const navigate=useNavigate();
+
   return (
     <div>
         <h2>404 | PAGE NOT FOUND</h2>
+        <br />
+        <button onClick={() => navigate('/')}>Go back to Home Page</button>
     </div>
   )
 }
