79e88ee97cccd2155cc602ed27c42391f37cfc8e
Vedant <vedant28j@gmail.com>
2025-07-15 11:17:47 +0000

    Using useLoaderData and jobDetailsLoader to display different job profiles

--- src/component/JobDetails.jsx
+++ src/component/JobDetails.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
 
 const JobDetails = () => {
 
-  const {id} = useParams()
+  // const {id} = useParams()
+  const jobDetails = useLoaderData();
 
   return (
-    <div>
-        <h1>Id is: {id}</h1>
+    <div className='jobDetails'>
+        {/* <h1>Id is: {id}</h1> */}
+        <p><b>Job Title: </b>{jobDetails.title}</p>
+        <p><b>Salary: </b>{jobDetails.salary}</p>
+        <p><b>Job Location: </b>{jobDetails.location}</p>
+        <p><b>Job Description: </b>hkashbcsvdkhbvkhbk webfkbwefwufy</p>
+        <button>Apply Now</button>
     </div>
   )
 }
 
 export default JobDetails
+
+export const JobDetailsLoader = async({params}) =>{
+  const {id} = params;
+  const res=await fetch("http://localhost:5000/jobs/"+id);
+  return res.json();
+}
