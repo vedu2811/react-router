9175c97c8edf1c60cae9c511c601a147f9c46166
Vedant <vedant28j@gmail.com>
2025-07-15 17:12:32 +0000

    Throwing error on jobs page

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -22,5 +22,8 @@ export default Jobs
 
 export const jobsLoader = async() =>{
     const res=await fetch("http://localhost:5000/jobs");
+    if(!res.ok){
+      throw Error('Could not find job list');
+    }
     return res.json();
 }
